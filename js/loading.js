c9bc4b2b9d6e16382f4e0afafae9b191cfe8935f
bartoszpieczynski <35772248+bartoszpieczynski@users.noreply.github.com>
2018-03-27 01:45:10 +0200

    Loading time

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,10 @@
-   setTimeout(function(){
-       $('#loader').fadeOut(1000);
-   }, 5000);
+(function ready() {
+  var interval = setInterval(function() {
+    if (document.readyState === "complete") {
+      if (document.readyState == "complete") {
+        $("#loader").fadeOut(1000);
+      }
+      clearInterval(interval);
+    }
+  }, 2000);
+})();
